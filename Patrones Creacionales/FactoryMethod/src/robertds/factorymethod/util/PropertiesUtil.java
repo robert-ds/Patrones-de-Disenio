94843037a105aa6a01cc08e0400c2ee436ff19a0
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-06-23 11:28:07 -0400

    create methods: loadProperty to class PropertiesUtil

--- Patrones Creacionales/FactoryMethod/src/robertds/factorymethod/util/PropertiesUtil.java
+++ Patrones Creacionales/FactoryMethod/src/robertds/factorymethod/util/PropertiesUtil.java
@@ -1,10 +1,29 @@
-package robertds.factorymethod.util;
-
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
  * Date: 22/6/22
  * Time: 11:09 a. m.
  */
+package robertds.factorymethod.util;
+
+import java.io.InputStream;
+import java.security.AuthProvider;
+import java.util.Properties;
+
 public class PropertiesUtil {
+
+  public static Properties loadProperty(String propertiesURL){
+    try{
+      Properties properties = new Properties();
+      InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream(propertiesURL);
+
+      properties.load(inputStream);
+      return properties;
+
+    }catch(Exception e){
+      e.printStackTrace();
+      return null;
+    }
+  }
+
 }
