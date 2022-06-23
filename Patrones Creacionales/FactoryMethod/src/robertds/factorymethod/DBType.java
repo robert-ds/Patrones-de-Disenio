545a4006b7dc8d87cb0cb7cc8b0c224d900df3b2
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-06-23 10:40:35 -0400

    create enum DBtype

--- Patrones Creacionales/FactoryMethod/src/robertds/factorymethod/DBType.java
+++ Patrones Creacionales/FactoryMethod/src/robertds/factorymethod/DBType.java
@@ -1,10 +1,11 @@
-package robertds.factorymethod;
-
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
  * Date: 22/6/22
  * Time: 11:03 a. m.
  */
-public class DBType {
+
+package robertds.factorymethod;
+public enum DBType {
+  MySQL, Oracle,
 }
