26ce7bec91206366744f977cba38181cdd88df7c
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-06-23 10:52:14 -0400

    create class product, construct, getters and setters

--- Patrones Creacionales/FactoryMethod/src/robertds/factorymethod/entity/Product.java
+++ Patrones Creacionales/FactoryMethod/src/robertds/factorymethod/entity/Product.java
@@ -1,10 +1,46 @@
-package robertds.factorymethod.entity;
-
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
  * Date: 22/6/22
  * Time: 11:06 a. m.
  */
+package robertds.factorymethod.entity;
+
 public class Product {
+  private Long idProduct;
+  private String productName;
+  private double price;
+
+  public Product(Long idProduct, String productName, double price){
+    this.idProduct = idProduct;
+    this.productName = productName;
+    this.price = price;
+  }
+
+  // Gets and Sets
+
+  public Long getIdProduct() {
+    return idProduct;
+  }
+
+  public void setIdProduct(Long idProduct) {
+    this.idProduct = idProduct;
+  }
+
+  public String getProducName(){
+    return productName;
+  }
+
+  public void setProductName(String productName){
+    this.productName = productName;
+  }
+
+  public double getPrice(){
+    return price;
+  }
+
+  public void setPrice(double price){
+    this.price = price;
+  }
+
 }
