502c7a167141f420f89b107a8c3b4c9c77af383a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-06-28 15:31:21 -0400

    class ProductServiceWSImpl implements Interface IProductSevice and too EmployeeServiceWSImpl

--- Patrones Creacionales/AbstractFactory/src/abstractfactory/ws/EmployeeServiceWSImpl.java
+++ Patrones Creacionales/AbstractFactory/src/abstractfactory/ws/EmployeeServiceWSImpl.java
@@ -1,10 +1,20 @@
 package abstractfactory.ws;
 
+import abstractfactory.service.IEmployeeService;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vasquez
  * Date: 28/6/22
  * Time: 2:30 p. m.
  */
-public class EmployeeServiceWSImpl {
+public class EmployeeServiceWSImpl implements IEmployeeService {
+
+  private static final String[] EMPLOYEES = new String[]{"Maria","Rebeca","Liliana"};
+
+  @Override
+  public String[] getEmployee() {
+    System.out.println("WebServices");
+    return EMPLOYEES;
+  }
 }
--- Patrones Creacionales/AbstractFactory/src/abstractfactory/ws/ProductServiceWSImpl.java
+++ Patrones Creacionales/AbstractFactory/src/abstractfactory/ws/ProductServiceWSImpl.java
@@ -1,10 +1,21 @@
 package abstractfactory.ws;
 
+import abstractfactory.service.IProductsService;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 28/6/22
  * Time: 2:30 p. m.
  */
-public class ProductServiceWSImpl {
+
+public class ProductServiceWSImpl implements IProductsService {
+
+  private static final String[] PRODUCTS = new String[]{"Refrsco","Jugo","Fruta"};
+
+  @Override
+  public String[] getProducts() {
+    System.out.println("WebService");
+    return PRODUCTS;
+  }
 }
