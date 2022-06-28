dfe51fd34fd66803605d9679eaa7c489e791d74d
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-06-28 15:22:10 -0400

    class implements ProductServiceRestImpl and EmployeeServiceRestImpl and method getProduct and getEmployee

--- Patrones Creacionales/AbstractFactory/src/abstractfactory/rest/EmployeeServiceRestImpl.java
+++ Patrones Creacionales/AbstractFactory/src/abstractfactory/rest/EmployeeServiceRestImpl.java
@@ -1,10 +1,21 @@
 package abstractfactory.rest;
 
+import abstractfactory.service.IEmployeeService;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 28/6/22
  * Time: 2:24 p. m.
  */
-public class EmployeeServiceRestImpl {
+
+public class EmployeeServiceRestImpl implements IEmployeeService {
+
+  private static final String[] EMPLOYEES = new String[]{"Juan","Manuel","Alberto"};
+
+  @Override
+  public String[] getEmployee() {
+    System.out.println("RestFul");
+    return EMPLOYEES;
+  }
 }
--- Patrones Creacionales/AbstractFactory/src/abstractfactory/rest/ProductServiceRestImpl.java
+++ Patrones Creacionales/AbstractFactory/src/abstractfactory/rest/ProductServiceRestImpl.java
@@ -1,10 +1,21 @@
 package abstractfactory.rest;
 
+import abstractfactory.service.IProductsService;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 28/6/22
  * Time: 2:25 p. m.
  */
-public class ProductServiceRestImpl {
+
+public class ProductServiceRestImpl implements IProductsService {
+
+  private static final String[] PRODUCTS = new String[]{"Teclado", "Monitor", "Mouse"};
+
+  @Override
+  public String[] getProducts() {
+    System.out.println("RestFul");
+    return PRODUCTS;
+  }
 }
