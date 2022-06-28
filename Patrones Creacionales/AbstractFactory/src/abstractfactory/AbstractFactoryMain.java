a8e9f65e0874c9cfb1ebc4a69716d3337b0e0b1f
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-06-28 16:11:09 -0400

    instance factory and executing AbstactFactoryMain

--- Patrones Creacionales/AbstractFactory/src/abstractfactory/AbstractFactoryMain.java
+++ Patrones Creacionales/AbstractFactory/src/abstractfactory/AbstractFactoryMain.java
@@ -2,6 +2,10 @@ package abstractfactory;
 
 import abstractfactory.impl.IServiceStackAbstractFactory;
 import abstractfactory.impl.ServiceStackAbstractFactory;
+import abstractfactory.service.IEmployeeService;
+import abstractfactory.service.IProductsService;
+
+import java.util.Arrays;
 
 /**
  * Created with IntelliJ IDEA
@@ -14,6 +18,14 @@ public class AbstractFactoryMain {
 
   public static void main(String[] args){
     IServiceStackAbstractFactory factory = ServiceStackAbstractFactory.createServiceFactory();
+    IEmployeeService employeeService = factory.getEmployeeService();
+    IProductsService productService = factory.getProductsService();
+
+    System.out.println("EmployeeService class => " + employeeService.getClass().getCanonicalName());
+    System.out.println("ProductService class => " + productService.getClass().getCanonicalName());
+
+    System.out.println("getEmployee => " + Arrays.toString(employeeService.getEmployee()));
+    System.out.println("getProduct => " + Arrays.toString(productService.getProducts()));
   }
 
 }
