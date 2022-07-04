99d6f45d3136a83cdfbf1a0cdf6507d9d1887936
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-04 16:45:59 -0400

    create class Contact, constructs and methods getters and setters

--- Patrones Creacionales/Builder/src/builder/dto/Contact.java
+++ Patrones Creacionales/Builder/src/builder/dto/Contact.java
@@ -2,9 +2,53 @@ package builder.dto;
 
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 4/7/22
  * Time: 3:40 p. m.
  */
+
 public class Contact {
+
+  private String name;
+  private Phone phone;
+  private Address address;
+
+  public Contact(){}
+
+  public Contact(String name, Phone phone, Address address){
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+  }
+
+  public Contact(String name, Phone phone){
+    this.name = name;
+    this.phone = phone;
+  }
+
+  // Getters and Setters
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public Phone getPhone() {
+    return phone;
+  }
+
+  public void setPhone(Phone phone) {
+    this.phone = phone;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
 }
