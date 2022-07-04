aa478a59a7a524c95da722cf8d5ebb25b4d8f26c
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-04 16:33:17 -0400

    create class Phone, constructs and methods getters and setters

--- Patrones Creacionales/Builder/src/builder/dto/Phone.java
+++ Patrones Creacionales/Builder/src/builder/dto/Phone.java
@@ -2,9 +2,48 @@ package builder.dto;
 
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 4/7/22
  * Time: 3:40 p. m.
  */
+
 public class Phone {
+
+  private String phoneNumber;
+  private String ext;
+  private String phoneType;
+
+  public Phone(){}
+
+  public Phone(String phoneNumber, String ext, String phoneType){
+    this.phoneNumber = phoneNumber;
+    this.ext = ext;
+    this.phoneType = phoneType;
+  }
+
+  // GETTERS and SETTERS
+
+  public String getPhoneNumber(){
+    return this.phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber){
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getExt() {
+    return ext;
+  }
+
+  public void setExt(String ext) {
+    this.ext = ext;
+  }
+
+  public String getPhoneType() {
+    return phoneType;
+  }
+
+  public void setPhoneType(String phoneType) {
+    this.phoneType = phoneType;
+  }
 }
