64c4d3afddd38cb15258248ee39f78045dfcdc84
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-05 14:41:44 -0400

    Create class PrototypeFactory and define methods getPrototype and addPrototype

--- Patrones Creacionales/Prototype/src/prototype/impl/PrototypeFactory.java
+++ Patrones Creacionales/Prototype/src/prototype/impl/PrototypeFactory.java
@@ -1,10 +1,24 @@
 package prototype.impl;
 
+import java.util.HashMap;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 5/7/22
  * Time: 2:24 p. m.
  */
+
 public class PrototypeFactory {
+
+  private static HashMap<String,IPrototype> prototypes = new HashMap<>();
+
+  public static IPrototype getPrototype(String prototypeName){
+    return prototypes.get(prototypeName).deepClone();
+  }
+
+  public static void addPrototype(String prototypeName, IPrototype prototype){
+    prototypes.put(prototypeName,prototype);
+  }
+
 }
