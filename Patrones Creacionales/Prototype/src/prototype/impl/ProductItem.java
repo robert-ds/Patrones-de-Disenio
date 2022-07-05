f8f7345c761a58ef6a5a2fd7e7aa091ab0359f5e
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-05 15:20:34 -0400

    create class ProductItem, method construct, Getters and Setters and implement interface IPrototype and implements methods clone and deepClone

--- Patrones Creacionales/Prototype/src/prototype/impl/ProductItem.java
+++ Patrones Creacionales/Prototype/src/prototype/impl/ProductItem.java
@@ -2,9 +2,58 @@ package prototype.impl;
 
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 5/7/22
  * Time: 2:24 p. m.
  */
-public class ProductItem {
+
+public class ProductItem implements IPrototype<ProductItem> {
+
+  private String name;
+  private double price;
+
+  public ProductItem(){}
+
+  public ProductItem(String name, double price){
+    this();
+    this.name = name;
+    this.price = price;
+  }
+
+  // Getters and Setters
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  @Override
+  public ProductItem clone() {
+    return new ProductItem(this.name, this.price);
+  }
+
+  @Override
+  public ProductItem deepClone() {
+    return clone();
+  }
+
+  @Override
+  public String toString() {
+    return "ProductItem{" +
+        "name='" + name + '\'' +
+        ", price=" + price +
+        '}';
+  }
+  
 }
