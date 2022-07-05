d1102f9e6ce3c9527da9c4ca7a4a9025ec0ec53a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-05 15:08:25 -0400

    create class PriceListImpl, construct method, getters and setters and implements Interface IPrototype and respective implements methods clone and deepClone

--- Patrones Creacionales/Prototype/src/prototype/impl/PriceListImpl.java
+++ Patrones Creacionales/Prototype/src/prototype/impl/PriceListImpl.java
@@ -1,10 +1,69 @@
 package prototype.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 5/7/22
  * Time: 2:23 p. m.
  */
-public class PriceListImpl {
+
+public class PriceListImpl implements IPrototype<PriceListImpl> {
+  private String listName;
+  private List<ProductItem> products = new ArrayList<ProductItem>();
+
+  public PriceListImpl(String listName){
+    this.listName = listName;
+  }
+
+  // Getters and Setters
+
+  public String getListName() {
+    return listName;
+  }
+
+  public void setListName(String listName) {
+    this.listName = listName;
+  }
+
+  public List<ProductItem> getProducts() {
+    return products;
+  }
+
+  public void setProducts(List<ProductItem> products) {
+    this.products = products;
+  }
+
+  @Override
+  public PriceListImpl clone(){
+    PriceListImpl clone = new PriceListImpl(listName);
+    clone.setProducts(products);
+    return clone;
+  }
+
+  @Override
+  public PriceListImpl deepClone() {
+    List<ProductItem> cloneProducts = new ArrayList<>();
+    for(ProductItem item: this.products){
+      ProductItem cloneItem = item.clone();
+      cloneProducts.add(cloneItem);
+    }
+    PriceListImpl clone = new PriceListImpl(listName);
+    clone.setProducts(cloneProducts);
+    return clone;
+  }
+
+  @Override
+  public String toString() {
+    String items = "";
+    for(ProductItem item: this.products){
+      items += "\t" + item.toString() + "\n";
+    }
+    return "PriceListImpl{" +
+        "listName='" + listName + '\'' +
+        ", products=\n" + items +
+        '}';
+  }
 }
