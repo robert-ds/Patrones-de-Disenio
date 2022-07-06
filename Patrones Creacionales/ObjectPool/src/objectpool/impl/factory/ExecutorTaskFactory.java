d5f1db28469f2b267b6829dfdff702988c85c186
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-06 16:29:39 -0400

    Class extends IPoolableObjectFactory and Override method ExecutorTask

--- Patrones Creacionales/ObjectPool/src/objectpool/impl/factory/ExecutorTaskFactory.java
+++ Patrones Creacionales/ObjectPool/src/objectpool/impl/factory/ExecutorTaskFactory.java
@@ -1,10 +1,17 @@
 package objectpool.impl.factory;
 
+import objectpool.impl.poolable.ExecutorTask;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 6/7/22
  * Time: 2:33 p. m.
  */
-public class ExecutorTaskFactory {
+
+public class ExecutorTaskFactory implements IPoolableObjectFactory<ExecutorTask> {
+  @Override
+  public ExecutorTask createNew() {
+    return new ExecutorTask();
+  }
 }
