8e1f851fd5cdd4e826a32005e86ab340b080d1f0
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-06 16:25:36 -0400

    Interface IPoolableObjectFactory with delegate the factory

--- Patrones Creacionales/ObjectPool/src/objectpool/impl/factory/IPoolableObjectFactory.java
+++ Patrones Creacionales/ObjectPool/src/objectpool/impl/factory/IPoolableObjectFactory.java
@@ -1,10 +1,14 @@
 package objectpool.impl.factory;
 
+import objectpool.impl.poolable.IPooledObject;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 6/7/22
  * Time: 2:33 p. m.
  */
-public interface IPoolableObjectFactory {
+
+public interface IPoolableObjectFactory<T extends IPooledObject> {
+  public T createNew();
 }
