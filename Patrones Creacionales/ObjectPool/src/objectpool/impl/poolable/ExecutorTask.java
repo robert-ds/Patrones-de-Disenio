d95b6d45d68bde1d987f4cee445f4a848b9b9c77
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-06 16:22:39 -0400

    Class ExecutorTask represent ConcretePoolableObject

--- Patrones Creacionales/ObjectPool/src/objectpool/impl/poolable/ExecutorTask.java
+++ Patrones Creacionales/ObjectPool/src/objectpool/impl/poolable/ExecutorTask.java
@@ -2,9 +2,42 @@ package objectpool.impl.poolable;
 
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 6/7/22
  * Time: 2:35 p. m.
  */
-public class ExecutorTask {
+
+public class ExecutorTask implements IPooledObject {
+
+  private int uses;
+  private static int invalidate;
+  private static int counter;
+
+  public void execute(){
+
+    int i = ++counter;
+
+    uses++;
+
+    System.out.println("Execute ==> "  + i );
+    try{
+      Thread.sleep(5000);
+    }catch(Exception e){
+
+    }
+
+    System.out.println("Execute end ==> " + i);
+
+  }
+
+  @Override
+  public boolean validate() {
+    return uses < 2;
+  }
+
+  @Override
+  public void invalidate() {
+    invalidate++;
+    System.out.println("Invalidadte Counter ==> " + invalidate);
+  }
 }
