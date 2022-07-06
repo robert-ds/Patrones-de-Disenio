ab3020f749ffb6f65089655ebb29b85f1e062e2a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-06 16:10:15 -0400

    Class represent the object pool finished and extends AbstactObjectPool

--- Patrones Creacionales/ObjectPool/src/objectpool/impl/ExecutorThreadPool.java
+++ Patrones Creacionales/ObjectPool/src/objectpool/impl/ExecutorThreadPool.java
@@ -1,10 +1,18 @@
 package objectpool.impl;
 
+import objectpool.impl.factory.IPoolableObjectFactory;
+import objectpool.impl.poolable.ExecutorTask;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 6/7/22
  * Time: 2:31 p. m.
  */
-public class ExecutorThreadPool {
+
+public class ExecutorThreadPool extends AbstractObjectPool<ExecutorTask>{
+  public ExecutorThreadPool(int minInstances, int maxInstances, int waitTime, IPoolableObjectFactory<ExecutorTask> poolableObjectFactory){
+      super(minInstances, maxInstances, waitTime, poolableObjectFactory);
+  }
+
 }
