9114ce4c1b45c8c7357e6f3d581ed44f88046552
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-06 16:13:57 -0400

    All objects the interface to pool implements

--- Patrones Creacionales/ObjectPool/src/objectpool/impl/poolable/IPooledObject.java
+++ Patrones Creacionales/ObjectPool/src/objectpool/impl/poolable/IPooledObject.java
@@ -2,9 +2,11 @@ package objectpool.impl.poolable;
 
 /**
  * Created with IntelliJ IDEA
- * Created By Kinsey
+ * Created By Robert Vásquez
  * Date: 6/7/22
  * Time: 2:36 p. m.
  */
 public interface IPooledObject {
+  public boolean validate();
+  public void invalidate();
 }
