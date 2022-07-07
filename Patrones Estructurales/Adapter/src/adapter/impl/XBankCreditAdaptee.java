bc83f8de2b08a0c5ed1bf2a650fc2c7fe73d7589
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-07 17:26:13 -0400

    Class Represent the adapter between Client and XBank

--- Patrones Estructurales/Adapter/src/adapter/impl/XBankCreditAdaptee.java
+++ Patrones Estructurales/Adapter/src/adapter/impl/XBankCreditAdaptee.java
@@ -1,5 +1,9 @@
 package adapter.impl;
 
+import adapter.creditapi.bankx.XBankCreditAPI;
+import adapter.creditapi.bankx.XBankCreditRequest;
+import adapter.creditapi.bankx.XBankCreditResponse;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +11,21 @@ package adapter.impl;
  * Time: 4:31 p. m.
  */
 
-public class XBankCreditAdaptee {
+public class XBankCreditAdaptee implements IBankAdapter {
+
+  @Override
+  public BankCreditResponse sendCreditRequest(BankCreditRequest request) {
+
+    XBankCreditRequest xrequest = new XBankCreditRequest();
+    xrequest.setCustomerName(request.getCustomer());
+    xrequest.setRequestAmount(request.getAmount());
+
+    XBankCreditAPI api = new XBankCreditAPI();
+    XBankCreditResponse xresponse = api.sendCreditRequest(xrequest);
+
+    BankCreditResponse response = new BankCreditResponse();
+    response.setApproved(xresponse.isAproval());
+    return response;
+
+  }
 }
