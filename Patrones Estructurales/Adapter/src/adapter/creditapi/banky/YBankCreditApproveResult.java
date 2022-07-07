905a797b0d01b0f359e04a983117eeb04eaa920f
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-07 18:41:00 -0400

    Class represent the response the request credit of YBank

--- Patrones Estructurales/Adapter/src/adapter/creditapi/banky/YBankCreditApproveResult.java
+++ Patrones Estructurales/Adapter/src/adapter/creditapi/banky/YBankCreditApproveResult.java
@@ -8,4 +8,17 @@ package adapter.creditapi.banky;
  */
 
 public class YBankCreditApproveResult {
+
+  public String approved;
+
+  // Getters and Setters
+
+  public String getApproved(){
+    return approved;
+  }
+
+  public void setApproved(){
+    this.approved = approved;
+  }
+
 }
