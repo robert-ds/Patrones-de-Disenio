40418b8b3d6aed2129b808105a62afb5aeaf179a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-07 17:47:48 -0400

    Class util for adapter between Client and the API of YBank

--- Patrones Estructurales/Adapter/src/adapter/impl/YBankCreditAdaptee.java
+++ Patrones Estructurales/Adapter/src/adapter/impl/YBankCreditAdaptee.java
@@ -1,5 +1,10 @@
 package adapter.impl;
 
+import adapter.creditapi.banky.YBankCreditApprove;
+import adapter.creditapi.banky.YBankCreditApproveResult;
+import adapter.creditapi.banky.YBankCreditSender;
+import adapter.creditapi.banky.YBankCreditSenderListener;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +12,39 @@ package adapter.impl;
  * Time: 4:31 p. m.
  */
 
-public class YBankCreditAdaptee {
+public class YBankCreditAdaptee implements IBankAdapter, YBankCreditSenderListener {
+
+  private YBankCreditApproveResult yresponse;
+
+  @Override
+  public BankCreditResponse sendCreditRequest(BankCreditRequest request) {
+    YBankCreditApprove yrequest = new YBankCreditApprove();
+    yrequest.setCredit((float) request.getAmount());
+    yrequest.setName(request.getCustomer());
+
+    YBankCreditSender sender = new YBankCreditSender();
+    sender.sendCreditForValidate(yrequest, this);
+
+    do{
+
+      try{
+        Thread.sleep(10000);
+        System.out.println("YBank petición en espera");
+      }catch(Exception e){
+        e.printStackTrace();
+      }
+
+    }while(yresponse == null);
+
+    BankCreditResponse response = new BankCreditResponse();
+    response.setApproved(yresponse.getApproved() == "Y" ? true : false);
+    return response;
+
+  }
+
+  @Override
+  public void notifyCreditResult(YBankCreditApproveResult result){
+    this.yresponse = result;
+  }
+
 }
