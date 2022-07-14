f05afc78d9072656fcb9f5fd573b451f9eebdd4b
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-14 15:54:58 -0400

    Class that implements IEncrypAlgorithm and encrypt message with algorithm DES

--- Patrones Estructurales/Bridge/src/bridge/encript/DESEncryptAlgorithm.java
+++ Patrones Estructurales/Bridge/src/bridge/encript/DESEncryptAlgorithm.java
@@ -1,5 +1,11 @@
 package bridge.encript;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import sun.misc.BASE64Encoder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,6 +13,19 @@ package bridge.encript;
  * Time: 3:25 p. m.
  */
 
-public class DESEncryptAlgorithm {
+public class DESEncryptAlgorithm  implements IEncryptAlgorithm{
+
+  @Override
+  public String encrypt(String message, String password) throws Exception {
+    DESKeySpec dks = new DESKeySpec(message.getBytes());
+    SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+    SecretKey desKey = skf.generateSecret(dks);
+    Cipher desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+    desCipher.init(Cipher.ENCRYPT_MODE, desKey);
+
+    BASE64Encoder base64encoder = new BASE64Encoder();
+    return base64encoder.encode(desCipher.doFinal(message.getBytes()));
+
+  }
 
 }
