7421fb2e3c9facb5f20e206c021efaf5a3c97852
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-14 16:02:12 -0400

    Class that implements IEncrypAlgorithm and encrypt message with algorithm AES

--- Patrones Estructurales/Bridge/src/bridge/encript/AESEncryptAlgorithm.java
+++ Patrones Estructurales/Bridge/src/bridge/encript/AESEncryptAlgorithm.java
@@ -1,5 +1,10 @@
 package bridge.encript;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import sun.misc.BASE64Encoder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Rober Vásquez
@@ -7,6 +12,18 @@ package bridge.encript;
  * Time: 3:24 p. m.
  */
 
-public class AESEncryptAlgorithm {
+public class AESEncryptAlgorithm implements IEncryptAlgorithm {
+
+  @Override
+  public String encrypt(String message, String password) throws Exception {
+    Key key = new SecretKeySpec(password.getBytes(), "AES");
+    Cipher c = Cipher.getInstance("AES");
+
+    c.init(Cipher.ENCRYPT_MODE, key);
+    byte[] encVal = c.doFinal(message.getBytes());
+    String encryptedValue = new BASE64Encoder().encode(encVal);
+    return encryptedValue;
+
+  }
 
 }
