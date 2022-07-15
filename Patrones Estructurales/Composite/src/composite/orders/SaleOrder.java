063c09c602884ef051817d149d7641701654f175
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-15 15:31:28 -0400

    Class that represent Sale order at very height level

--- Patrones Estructurales/Composite/src/composite/orders/SaleOrder.java
+++ Patrones Estructurales/Composite/src/composite/orders/SaleOrder.java
@@ -1,5 +1,12 @@
 package composite.orders;
 
+import composite.products.AbstractProduct;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,5 +15,66 @@ package composite.orders;
  */
 
 public class SaleOrder {
+
+  private long orderId;
+  private String customer;
+  private List<AbstractProduct> products = new ArrayList<AbstractProduct>();
+
+  public SaleOrder(long orderId, String customer){
+    super();
+    this.orderId = orderId;
+    this.customer = customer;
+  }
+
+  public double getPrice(){
+    double price = 0d;
+
+    for(AbstractProduct child: products){
+      price += child.getPrice();
+    }
+
+    return price;
+  }
+
+  public void addProduct(AbstractProduct product){
+    products.add(product);
+  }
+
+  public void printOrder(){
+    NumberFormat formater = new DecimalFormat("###,##0.00");
+    System.out.println("\\\n============================================" + "\nOrden: " + orderId + "\nCliente: " + customer + "\nProductos:\n");
+
+    for(AbstractProduct prod: products){
+      System.out.println(prod.getName() + "\t\t\t$ " + formater.format(prod.getPrice()));
+    }
+    System.out.println("Total: " + formater.format(getPrice()));
+
+  }
+
+  // Getters and Setters
+
+  public long getOrderId(){
+    return this.orderId;
+  }
+
+  public void setOrderId(long orderId){
+    this.orderId = orderId;
+  }
+
+  public String getCustomer(){
+    return this.customer;
+  }
+
+  public void setCustomer(String customer){
+    this.customer = customer;
+  }
+
+  public List<AbstractProduct> getProducts(){
+    return this.products;
+  }
+
+  public void setProduct(List<AbstractProduct> products){
+    this.products = products;
+  }
 
 }
