ff224bb5824465e192e86c4c4c8e5aae6dba8898
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-15 14:30:35 -0400

    Class that represent a simple product

--- Patrones Estructurales/Composite/src/composite/products/SimpleProduct.java
+++ Patrones Estructurales/Composite/src/composite/products/SimpleProduct.java
@@ -7,5 +7,22 @@ package composite.products;
  * Time: 2:15 p. m.
  */
 
-public class SimpleProduct {
+public class SimpleProduct extends AbstractProduct {
+
+  protected String brand;
+  public SimpleProduct(String name, double price, String brand) {
+    super(name, price);
+    this.brand = brand;
+  }
+
+  //Getters and Setters
+
+  public String getBrand() {
+    return this.brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
 }
