7bca217679a4f55788043b5e5a603c095102853c
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-18 14:29:15 -0400

    Implemets de IMessage for create message with the data of client

--- Patrones Estructurales/Decorator/src/decorator/impl/message/CustomerMessage.java
+++ Patrones Estructurales/Decorator/src/decorator/impl/message/CustomerMessage.java
@@ -6,5 +6,61 @@ package decorator.impl.message;
  * Date: 18/7/22
  * Time: 2:08 p. m.
  */
-public class CustomerMessage {
+
+public class CustomerMessage implements IMessage {
+
+  private String name;
+  private String email;
+  private String telephone;
+
+  @Override
+  public IMessage processMessage() {
+    return this;
+  }
+
+  @Override
+  public String getContent() {
+    return toString();
+  }
+
+  @Override
+  public void setContent(String content) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  @Override
+  public String toString() {
+    return "QueryCustomerMessage{" +
+        "name: '" + name + '\'' +
+        ", email: '" + email + '\'' +
+        ", telephone: '" + telephone + '\'' +
+        '}';
+  }
+
+  //Getters and Setters
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+  public void setEmail(){
+    this.email = email;
+  }
+
+  public String getTelephone(){
+    return this.telephone;
+  }
+
+  public void setTelephone(String telephone){
+    this.telephone = telephone;
+  }
+
 }
