b0c66e0c664cf00177b0c9dbfbee7b300406319b
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-18 14:46:53 -0400

    Class implements that base for create the to decorators, this get in the constructor an IMessage

--- Patrones Estructurales/Decorator/src/decorator/impl/decorators/MessageDecorator.java
+++ Patrones Estructurales/Decorator/src/decorator/impl/decorators/MessageDecorator.java
@@ -1,12 +1,29 @@
 package decorator.impl.decorators;
 
+import decorator.impl.message.IMessage;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Rober Vásquez
+ * Created By Robert Vásquez
  * Date: 18/7/22
  * Time: 2:05 p. m.
  */
 
-public class MessageDecorator {
+public abstract class MessageDecorator implements IMessage {
 
+  protected IMessage message;
+
+  public MessageDecorator(IMessage message){
+    this.message = message;
+  }
+
+  @Override
+  public String getContent() {
+    return message.getContent();
+  }
+
+  @Override
+  public void setContent(String content) {
+    message.setContent(content);
+  }
 }
