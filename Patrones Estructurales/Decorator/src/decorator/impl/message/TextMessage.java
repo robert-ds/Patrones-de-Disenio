6bab40f10510be89b69afe5153a422884e028509
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-18 14:40:12 -0400

    Class that create the text message, the message is save in propierties content

--- Patrones Estructurales/Decorator/src/decorator/impl/message/TextMessage.java
+++ Patrones Estructurales/Decorator/src/decorator/impl/message/TextMessage.java
@@ -7,6 +7,26 @@ package decorator.impl.message;
  * Time: 2:10 p. m.
  */
 
-public class TextMessage {
+public class TextMessage implements IMessage {
+
+  private String content;
+
+  // Construct
+  public TextMessage(){}
+
+  @Override
+  public IMessage processMessage() {
+    return this;
+  }
+
+  @Override
+  public String getContent() {
+    return this.content;
+  }
+
+  @Override
+  public void setContent(String content) {
+    this.content = content;
+  }
 
 }
