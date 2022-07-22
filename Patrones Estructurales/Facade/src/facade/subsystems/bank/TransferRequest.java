a682c3ec2fc87080c8f45debdb7e0ed464aec032
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-22 16:20:39 -0400

    Class represent the request for system bank, that is receives the data of card and ammount

--- Patrones Estructurales/Facade/src/facade/subsystems/bank/TransferRequest.java
+++ Patrones Estructurales/Facade/src/facade/subsystems/bank/TransferRequest.java
@@ -9,4 +9,59 @@ package facade.subsystems.bank;
 
 public class TransferRequest {
 
+  private double ammount;
+  private String cardNumber;
+  private String cardName;
+  private String cardExpDate;
+  private String cardSecureNum;
+
+  // Cosntruct
+  public TransferRequest(double ammount, String cardNumber, String cardName, String cardExpDate, String cardSecureNum){
+    this.ammount = ammount;
+    this.cardNumber = cardNumber;
+    this.cardName = cardName;
+    this.cardExpDate = cardExpDate;
+    this.cardSecureNum = cardSecureNum;
+  }
+
+  // Getters and Setters
+  public double getAmmount(){
+    return this.ammount;
+  }
+
+  public void setAmmount(double ammount){
+    this.ammount = ammount;
+  }
+
+  public String getCardNumber() {
+    return this.cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getCardName() {
+    return this.cardName;
+  }
+
+  public void setCardName(String cardName) {
+    this.cardName = cardName;
+  }
+
+  public String getCardExpDate() {
+    return this.cardExpDate;
+  }
+
+  public void setCardExpDate(String cardExpDate) {
+    this.cardExpDate = cardExpDate;
+  }
+
+  public String getCardSecureNum() {
+    return this.cardSecureNum;
+  }
+
+  public void setCardSecureNum(String cardSecureNum) {
+    this.cardSecureNum = cardSecureNum;
+  }
 }
