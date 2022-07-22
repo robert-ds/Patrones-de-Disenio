2a2462ff4d6cd571791907df18e8df58e9655730
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-22 16:56:00 -0400

    Class represent request for the system of Billing

--- Patrones Estructurales/Facade/src/facade/subsystems/biller/BillingPayRequest.java
+++ Patrones Estructurales/Facade/src/facade/subsystems/biller/BillingPayRequest.java
@@ -9,4 +9,31 @@ package facade.subsystems.biller;
 
 public class BillingPayRequest {
 
+  private Long customerId;
+  private double amount;
+
+  // Construct
+  public BillingPayRequest(Long customerId, double amount){
+    this.customerId = customerId;
+    this.amount = amount;
+  }
+
+  // Getters and Setters
+
+  public Long getCustomerId(){
+    return this.customerId;
+  }
+
+  public void setCustomerId(Long customerId){
+    this.customerId = customerId;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
+  
 }
