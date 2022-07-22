56e8856340fe3fe684b6c2a13d686a354a70db60
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-22 15:34:01 -0400

    Class corresponds to request for operation pay of the facade

--- Patrones Estructurales/Facade/src/facade/impl/PaymentRequest.java
+++ Patrones Estructurales/Facade/src/facade/impl/PaymentRequest.java
@@ -8,5 +8,63 @@ package facade.impl;
  */
 
 public class PaymentRequest {
+  private Long customerId;
+  private double ammount;
+  private String cardNumber;
+  private String cardName;
+  private String cardExpDate;
+  private String cardSecureNum;
 
+  // Construct
+  public PaymentRequest(){}
+
+  // Getters and Setters
+  public Long getCustomerId(){
+    return this.customerId;
+  }
+
+  public void setCustomerId(Long customerId){
+    this.customerId = customerId;
+  }
+
+  public double getAmmount() {
+    return this.ammount;
+  }
+
+  public void setAmmount(double ammount) {
+    this.ammount = ammount;
+  }
+
+  public String getCardNumber() {
+    return this.cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getCardName() {
+    return this.cardName;
+  }
+
+  public void setCardName(String cardName) {
+    this.cardName = cardName;
+  }
+
+  public String getCardExpDate() {
+    return this.cardExpDate;
+  }
+
+  public void setCardExpDate(String cardExpDate) {
+    this.cardExpDate = cardExpDate;
+  }
+
+  public String getCardSecureNum() {
+    return this.cardSecureNum;
+  }
+
+  public void setCardSecureNum(String cardSecureNum) {
+    this.cardSecureNum = cardSecureNum;
+  }
+  
 }
