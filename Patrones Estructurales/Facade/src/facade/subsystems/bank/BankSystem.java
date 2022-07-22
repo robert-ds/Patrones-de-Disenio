b4363e5a1109965c7f57b09af9b30addc79326a0
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-22 16:43:27 -0400

    Class represent the system bank, this offers a method call transfer that receives the data for realize the charge to card

--- Patrones Estructurales/Facade/src/facade/subsystems/bank/BankSystem.java
+++ Patrones Estructurales/Facade/src/facade/subsystems/bank/BankSystem.java
@@ -1,5 +1,10 @@
 package facade.subsystems.bank;
 
+import facade.impl.GeneralPaymentError;
+import facade.util.OnMemoryDataBase;
+
+import java.util.UUID;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,5 +13,27 @@ package facade.subsystems.bank;
  */
 
 public class BankSystem {
+
+  public String transfer(TransferRequest request) throws GeneralPaymentError {
+
+    String cardNumberPrefix = request.getCardNumber().substring(0,3);
+    if(!OnMemoryDataBase.validateCardBins(cardNumberPrefix)){
+      throw new GeneralPaymentError("Targeta invalida.");
+    }
+
+    String cardCompany = OnMemoryDataBase.getCardCompany(cardNumberPrefix);
+    if("Venture".equals(cardCompany) && request.getCardNumber().length() != 15){
+      throw new GeneralPaymentError("El número del la targeta es inválido");
+    }else if(("VISA".equals(cardCompany) || "MASTERCARD".equals(cardCompany)) && request.getCardNumber().length() != 16 ){
+      throw new GeneralPaymentError("El número de la targeta es inválido");
+    }
+
+    String number = request.getCardNumber();
+    String cardNumberSubfix = number.substring(number.length() - 4, number.length());
+    System.out.println("Se ha realizado un cargo al cliente " + request.getCardName() + "\n" + "\t por el monto de  " + request.getAmmount() + " a la targeta con terminacion " + cardNumberSubfix + ".\n");
+
+    return UUID.randomUUID().toString();
+
+  }
 
 }
