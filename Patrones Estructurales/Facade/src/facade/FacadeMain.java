edf9d1b279f33fbab64e5d80a0332bd4bd560c13
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-07-22 17:21:24 -0400

    This class FacadeMain for exec app and class Customer for define attributes and methods getters and setters

--- Patrones Estructurales/Facade/src/facade/FacadeMain.java
+++ Patrones Estructurales/Facade/src/facade/FacadeMain.java
@@ -1,5 +1,9 @@
 package facade;
 
+import facade.impl.IPaymentFacade;
+import facade.impl.OnlinePaymentFacadeImpl;
+import facade.impl.PaymentRequest;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -10,6 +14,21 @@ package facade;
 public class FacadeMain {
 
   public static void main(String[] args){
+
+    PaymentRequest request = new PaymentRequest();
+    request.setAmmount(500);
+    request.setCardExpDate("10/2021");
+    request.setCardName("Robert Vásquez");
+    request.setCardNumber("82742974928234");
+    request.setCardSecureNum("343");
+    request.setCustomerId(1L);
+
+    try{
+      IPaymentFacade paymentFacade = new OnlinePaymentFacadeImpl();
+      paymentFacade.pay(request);
+    }catch(Exception e){
+      System.out.println(e.getMessage());
+    }
 
   }
 
--- Patrones Estructurales/Facade/src/facade/util/Customer.java
+++ Patrones Estructurales/Facade/src/facade/util/Customer.java
@@ -9,4 +9,63 @@ package facade.util;
 
 public class Customer {
 
+  private Long id;
+  private String name;
+  private double balance;
+  private String status;
+
+  // Empty Construct
+  public Customer(){}
+
+  // Overrride Construct
+  public Customer(Long id, String name, double balance, String status){
+    this.id = id;
+    this.name = name;
+    this.balance = balance;
+    this.status = status;
+  }
+
+  // Getters and Setters
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  @Override
+  public String toString() {
+    return "Customer{" +
+        "id: " + id +
+        ", name: '" + name + '\'' +
+        ", balance: " + balance +
+        ", status: '" + status + '\'' +
+        '}';
+  }
 }
