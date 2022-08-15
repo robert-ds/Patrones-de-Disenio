a73b9a5c7e1447ea09815b00194832f6ed159076
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-15 15:33:19 -0400

    Class that represent the part intrinsic and that we will can reuse

--- Patrones Estructurales/Flyweight/src/flyweight/impl/PlayItem.java
+++ Patrones Estructurales/Flyweight/src/flyweight/impl/PlayItem.java
@@ -8,4 +8,42 @@ package flyweight.impl;
  */
 
 public class PlayItem {
+
+  private Long id;
+  private String songName;
+  private byte[] song = new byte[1000000];
+
+  public PlayItem(Long id, String songName){
+    this.id       = id;
+    this.songName = songName;
+  }
+
+  public PlayItem(){}
+
+  @Override
+  public String toString() {
+    return "PlayItem{" +
+        "id: " + id +
+        ", songName: '" + songName + '\'' +
+        '}';
+  }
+
+  // Getters and Setters
+
+  public Long getId(){
+    return this.id;
+  }
+
+  public void setId(Long id){
+    this.id = id;
+  }
+
+  public String getSongName(){
+    return songName;
+  }
+
+  public void setSongName(String songName){
+    this.songName = songName;
+  }
+
 }
