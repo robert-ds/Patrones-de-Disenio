69baa68a7aa72e94e5f7a3f35fa3fe4d8098d85c
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-15 16:01:52 -0400

    This class represent the PlayList

--- Patrones Estructurales/Flyweight/src/flyweight/impl/PlayList.java
+++ Patrones Estructurales/Flyweight/src/flyweight/impl/PlayList.java
@@ -1,5 +1,8 @@
 package flyweight.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,4 +11,45 @@ package flyweight.impl;
  */
 
 public class PlayList {
+
+  private String playListName;
+  private List<PlayItem> playItems = new ArrayList<>();
+
+  public PlayList(String playListName){
+    this.playListName = playListName;
+  }
+
+  public void addPlayItem(String songName){
+    playItems.add(PlayItemFactory.createPlayItem(songName));
+  }
+
+  public void printList(){
+    String out = "\nPlayList > " + playListName;
+
+    for(PlayItem playItem: playItems){
+      out += "\n\t " + playItem.toString();
+    }
+
+    System.out.println(out);
+
+  }
+
+  // Getter and Setters
+
+  public String getPlayListName(){
+    return this.playListName;
+  }
+
+  public void setPlayListName(String playListName){
+    this.playListName = playListName;
+  }
+
+  public List<PlayItem> getPlayItems(){
+    return this.playItems;
+  }
+
+  public void setPlayItems(List<PlayItem> playItems){
+    this.playItems = playItems;
+  }
+
 }
