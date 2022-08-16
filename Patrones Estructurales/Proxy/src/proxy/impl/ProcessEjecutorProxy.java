2280cf24fba48b36c07645536dbb09d95723637d
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-16 17:33:19 -0400

    The class used what Proxy, this class implements IProcessEjecutor

--- Patrones Estructurales/Proxy/src/proxy/impl/ProcessEjecutorProxy.java
+++ Patrones Estructurales/Proxy/src/proxy/impl/ProcessEjecutorProxy.java
@@ -1,5 +1,8 @@
 package proxy.impl;
 
+import proxy.services.AuditService;
+import proxy.services.SecurityService;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,22 @@ package proxy.impl;
  * Time: 4:54 p. m.
  */
 
-public class ProcessEjecutorProxy {
+public class ProcessEjecutorProxy implements IProcessEjecutor {
+
+  @Override
+  public void ejecuteProcess(int idProcess, String user, String password) throws Exception {
+
+    SecurityService securityService = new SecurityService();
+    if(!securityService.authorization(user, password)){
+      throw new Exception("El usuario " + user + "No tiene privilegios para ejecutar el proceso");
+    }
+
+    DefaultProcessEjecutor ejecutorProcess = new DefaultProcessEjecutor();
+    ejecutorProcess.ejecuteProcess(idProcess, user, password);
+
+    AuditService audit = new AuditService();
+    audit.auditServiceUsed(user, DefaultProcessEjecutor.class.getName());
+
+  }
+
 }
