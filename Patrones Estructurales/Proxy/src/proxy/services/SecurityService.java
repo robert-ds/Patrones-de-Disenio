aee5f759a53fdd593571d5f336837ab06d981295
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-16 17:12:13 -0400

    Class that simulate a security service and provides a authorization method

--- Patrones Estructurales/Proxy/src/proxy/services/SecurityService.java
+++ Patrones Estructurales/Proxy/src/proxy/services/SecurityService.java
@@ -9,4 +9,17 @@ package proxy.services;
 
 
 public class SecurityService {
+
+  public boolean authorization(String user, String password){
+
+    if(user.equals("robert") && password.equals("12345")){
+      System.out.println("Usuario" + user + " autorizado");
+      return true;
+    }else{
+      System.out.println("Usuario "  + user + "No autorizado");
+      return false;
+    }
+
+  }
+
 }
