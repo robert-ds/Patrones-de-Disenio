ee9a4444632a8caa8fa2191d0b06e19be960bc66
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-17 14:40:37 -0400

    Class employee that create employee and Class IteratorMain for exec app

--- Patrones de Comportamiento/Iterator/src/Iterator/IteratorMain.java
+++ Patrones de Comportamiento/Iterator/src/Iterator/IteratorMain.java
@@ -1,5 +1,8 @@
 package Iterator;
 
+import Iterator.impl.Employee;
+import Iterator.impl.IIterator;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -10,6 +13,26 @@ package Iterator;
 public class IteratorMain {
 
   public static void main(String[] args){
+
+    Employee employee = new Employee("Juan", "CEO",
+        new Employee("Pedro","Presidente",
+          new Employee("Liliana","VP",
+            new Employee("Oscar", "Gerente",
+              new Employee("Mario","Developer"),
+              new Employee("Rodolfo","Developer")),
+            new Employee("Sofia", "Gerente",
+              new Employee("Adrián","Sr Developer"),
+              new Employee("Rebeca", "Developer")
+            )
+          )
+        )
+        );
+
+    IIterator<Employee> empIterator = employee.createIterator();
+    while(empIterator.hasNext()){
+      Employee emp = empIterator.next();
+      System.out.println("emp >" + emp.toString());
+    }
 
   }
 
