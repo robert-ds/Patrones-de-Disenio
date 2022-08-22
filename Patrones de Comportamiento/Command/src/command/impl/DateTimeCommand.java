c0be98a2b98efc0c31bffcb38f0a75ee58ae3db3
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-22 18:43:32 -0400

    The class DateTimeCommand implement the command date and allow know the date

--- Patrones de Comportamiento/Command/src/command/impl/DateTimeCommand.java
+++ Patrones de Comportamiento/Command/src/command/impl/DateTimeCommand.java
@@ -1,5 +1,9 @@
 package command.impl;
 
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +11,33 @@ package command.impl;
  * Time: 3:36 p. m.
  */
 
-public class DateTimeCommand {
+public class DateTimeCommand extends BaseCommand{
+
+  public static final String COMMAND_NAME = "date";
+
+  @Override
+  public String getCommandName() {
+    return COMMAND_NAME;
+  }
+
+  @Override
+  public String execute(String[] args, OutputStream out) {
+    SimpleDateFormat dateFormater = null;
+
+    if(args == null){
+      dateFormater =new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+    }else{
+      try{
+        dateFormater = new SimpleDateFormat(args[0]);
+      }catch(Exception e){
+        write(out,"Formato invalido");
+        return "";
+      }
+    }
+
+    String date = dateFormater.format(new Date());
+    write(out,date);
+
+    return date;
+  }
 }
