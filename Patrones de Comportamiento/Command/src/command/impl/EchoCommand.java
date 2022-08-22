53c4a7af1a452359eff4b0a8fbde8556a6816731
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-22 18:32:34 -0400

    The class EchoCommand implement the command echo, which repeat in screen the send parameter

--- Patrones de Comportamiento/Command/src/command/impl/EchoCommand.java
+++ Patrones de Comportamiento/Command/src/command/impl/EchoCommand.java
@@ -1,5 +1,8 @@
 package command.impl;
 
+import java.io.OutputStream;
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,19 @@ package command.impl;
  * Time: 3:37 p. m.
  */
 
-public class EchoCommand {
+public class EchoCommand  extends BaseCommand{
+
+  public static final String COMMAN_NAME = "echo";
+
+  @Override
+  public String getCommandName() {
+    return COMMAN_NAME;
+  }
+
+  @Override
+  public String execute(String[] args, OutputStream out) {
+    String message = getCommandName() + "" + Arrays.toString(args);
+    write(out, message);
+  }
+
 }
