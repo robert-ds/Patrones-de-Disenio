159a47e08e18a7c360325c79da41f262a5cfc194
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-22 19:45:20 -0400

    The class process data key for users

--- Patrones de Comportamiento/Command/src/command/CommandUtil.java
+++ Patrones de Comportamiento/Command/src/command/CommandUtil.java
@@ -1,5 +1,8 @@
 package command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,4 +11,38 @@ package command;
  */
 
 public class CommandUtil {
+  public static String[] tokenizerArgs(String args){
+    List<String> tokens = new ArrayList<String>();
+    char[] charArray = args.toCharArray();
+
+    String contact = "";
+    boolean inText = false;
+    for (char c : charArray) {
+      if (c == ' ' && !inText) {
+        if (contact.length() != 0) {
+          tokens.add(contact);
+          contact = "";
+        }
+      } else if (c == '"') {
+        if (inText) {
+          tokens.add(contact);
+          contact = "";
+          inText = false;
+        } else {
+          inText = true;
+        }
+      } else {
+        contact += c;
+      }
+    }
+    if (contact.trim().length() != 0) {
+      tokens.add(contact.trim());
+    }
+
+    String[] argsArray = new String[tokens.size()];
+    argsArray = tokens.toArray(argsArray);
+    return argsArray;
+
+  }
+
 }
