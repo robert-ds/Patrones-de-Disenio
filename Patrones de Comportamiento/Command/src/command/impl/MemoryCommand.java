b77a3bbda406ea6c7aa9016086719a669d0dd4d5
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-22 18:51:37 -0400

    The class MemoryCommand implement the command memory which not parameters and print memory used for app

--- Patrones de Comportamiento/Command/src/command/impl/MemoryCommand.java
+++ Patrones de Comportamiento/Command/src/command/impl/MemoryCommand.java
@@ -1,5 +1,7 @@
 package command.impl;
 
+import java.io.OutputStream;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +9,24 @@ package command.impl;
  * Time: 3:39 p. m.
  */
 
-public class MemoryCommand {
+public class MemoryCommand extends BaseCommand{
+
+  public static final String COMMAND_NAME = "memory";
+
+  @Override
+  public String getCommandName() {
+    return COMMAND_NAME;
+  }
+
+  @Override
+  public String execute(String[] args, OutputStream out) {
+    double heap = Runtime.getRuntime().totalMemory() / 1000000d;
+    double heapMx = Runtime.getRuntime().maxMemory() / 1000000d;
+    double heapFree = Runtime.getRuntime().freeMemory() / 1000000d;
+
+    String salida = "Head: " + heap + "\nMax Heap: " + heapMx + "\nFree heap: " + heapFree;
+    write(out, salida);
+    
+    return salida;
+  }
 }
