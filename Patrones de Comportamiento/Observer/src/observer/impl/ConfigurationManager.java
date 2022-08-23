ee85a087c05963066aff1962e189f188713ccff6
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-23 17:01:41 -0400

    The class ConfigurationManager is the class that observer

--- Patrones de Comportamiento/Observer/src/observer/impl/ConfigurationManager.java
+++ Patrones de Comportamiento/Observer/src/observer/impl/ConfigurationManager.java
@@ -1,5 +1,8 @@
 package observer.impl;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,39 @@ package observer.impl;
  * Time: 4:26 p. m.
  */
 
-public class ConfigurationManager {
+public class ConfigurationManager extends AbstractObservable {
+
+  private SimpleDateFormat defaultDateFormat;
+  private NumberFormat moneyFormat;
+
+  private static ConfigurationManager configurationManager;
+
+  private ConfigurationManager(){
+
+  }
+
+  public static ConfigurationManager getInstance(){
+    if(configurationManager == null){
+      configurationManager = new ConfigurationManager();
+    }
+    return configurationManager;
+  }
+
+  public SimpleDateFormat getDefaultDateFormat(){
+    return defaultDateFormat;
+  }
+
+  public void setDefaultDateFormat(SimpleDateFormat defaultDateFormat){
+    this.defaultDateFormat = defaultDateFormat;
+  }
+
+  public NumberFormat getMoneyFormat(){
+    return moneyFormat;
+  }
+
+  public void setMoneyFormat(NumberFormat moneyFormat){
+    this.moneyFormat = moneyFormat;
+    notifyAllObservers("moneyFormat",this);
+  }
+
 }
