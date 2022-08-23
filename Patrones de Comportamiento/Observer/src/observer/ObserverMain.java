5229a389bdb904983e9511c54a9aa87b3059785d
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-23 17:39:23 -0400

    The class ObserverMain define the configure for exec app

--- Patrones de Comportamiento/Observer/src/observer/ObserverMain.java
+++ Patrones de Comportamiento/Observer/src/observer/ObserverMain.java
@@ -1,5 +1,11 @@
 package observer;
 
+import observer.impl.ConfigurationManager;
+import observer.impl.observers.DateFormatObserver;
+import observer.impl.observers.MoneyFormatObserver;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -10,6 +16,37 @@ package observer;
 public class ObserverMain {
 
   public static void main(String[] args){
+
+    ConfigurationManager conf = ConfigurationManager.getInstance();
+
+    // Valores Por Default
+    conf.setDefaultDateFormat(new SimpleDateFormat("yyyy/MM/dd"));
+    conf.setMoneyFormat(new DecimalFormat("##.00"));
+    System.out.println("Configuración Establecida");
+
+    // se Establecen los Observers
+    DateFormatObserver dateFormatObserver = new DateFormatObserver();
+    MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
+    conf.addObsever(dateFormatObserver);
+    conf.addObsever(moneyFormatObserver);
+    System.out.println("");
+
+    // Se cambia la configuracion
+    conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
+    conf.setMoneyFormat(new DecimalFormat("##,#00.00"));
+    System.out.println("");
+
+    // Se realiza otro cambio en la configuracion
+    conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy/dd"));
+    conf.setMoneyFormat(new DecimalFormat("###.00"));
+
+    conf.removeObserver(dateFormatObserver);
+    conf.removeObserver(moneyFormatObserver);
+    System.out.println("");
+
+    // se realiza otro cambio en la configuracion
+    conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy"));
+    conf.setMoneyFormat(new DecimalFormat("###,##0.00"));
 
   }
 
