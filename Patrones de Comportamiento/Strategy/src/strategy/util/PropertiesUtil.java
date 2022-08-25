4967b6e98ff8cdc65c2b95c7a0008640aca87e61
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-25 17:18:21 -0400

    Define the mode get properties

--- Patrones de Comportamiento/Strategy/src/strategy/util/PropertiesUtil.java
+++ Patrones de Comportamiento/Strategy/src/strategy/util/PropertiesUtil.java
@@ -1,5 +1,8 @@
 package strategy.util;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,4 +11,21 @@ package strategy.util;
  */
 
 public class PropertiesUtil {
+
+  public static Properties loadProperty(String propertiesURL){
+
+    try{
+      Properties properties = new Properties();
+      InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream(propertiesURL);
+      properties.load(inputStream);
+
+      return properties;
+
+    }catch (Exception e){
+      e.printStackTrace();
+      return null;
+    }
+
+  }
+
 }
