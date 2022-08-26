87a10e201fbf863211a80e61b085d186dfd89ee8
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 17:34:17 -0400

    Class that extends of AbstractOrder validator and will be used for validate that taxpayer of the order be a customer

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/CustomerValidator.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/CustomerValidator.java
@@ -1,5 +1,8 @@
 package chainofresponsability.validator;
 
+import chainofresponsability.domain.Customer;
+import chainofresponsability.domain.order.AbstractOrder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,16 @@ package chainofresponsability.validator;
  * Time: 3:43 p. m.
  */
 
-public class CustomerValidator {
+public class CustomerValidator extends AbstractOrderValidator {
+  @Override
+  public void validate(AbstractOrder order) throws ValidationException {
+    for(AbstractOrderValidator validator: validators){
+      validator.validate(order);
+    }
+
+    if(!(order.getContributor() instanceof Customer)){
+      throw new ValidationException("El contribuyente no es un cliente");
+    }
+
+  }
 }
