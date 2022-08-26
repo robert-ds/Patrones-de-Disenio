0cc5154b68bf71004927b73dd88ce9065ac6c78a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 17:13:50 -0400

    Class that extends of AbstractOrderValidator and it allows validate the data of a home through the method validate

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/AddressValidator.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/AddressValidator.java
@@ -1,5 +1,8 @@
 package chainofresponsability.validator;
 
+import chainofresponsability.domain.Address;
+import chainofresponsability.domain.order.AbstractOrder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,17 @@ package chainofresponsability.validator;
  * Time: 3:41 p. m.
  */
 
-public class AddressValidator {
+public class AddressValidator extends AbstractOrderValidator{
+  @Override
+  public void validate(AbstractOrder order) throws ValidationException {
+    Address address = order.getContributor().getAddress();
+    
+    if(address.getAddress1() == null || address.getAddress1().length() == 0){
+      throw new ValidationException("La dirección 1 es obligatoria");
+    }else if(address.getCP() == null || address.getCP().length() != 4){
+      throw new ValidationException("El CP debe de ser de 4 dígitos");
+    }else if(address.getCountry() == null || address.getCountry().length() == 0){
+      throw new ValidationException("El paí es obligatorio");;
+    }
+  }
 }
