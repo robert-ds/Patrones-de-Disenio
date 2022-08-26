5f84acfd03cefb24cd5bfb48b7c7d98ea80366cc
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 16:17:06 -0400

    This class containt the information of credit of client

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/domain/CreditData.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/domain/CreditData.java
@@ -8,4 +8,26 @@ package chainofresponsability.domain;
  */
 
 public class CreditData {
+
+  private double creditLimit;
+  private double balance;
+
+  // Getters and Setters
+
+  public double getCreditLimit(){
+    return this.creditLimit;
+  }
+
+  public void setCreditLimit(double creditLimit){
+    this.creditLimit = creditLimit;
+  }
+
+  public double getBalance(){
+    return this.balance;
+  }
+
+  public void setBalance(){
+    this.balance = balance;
+  }
+
 }
