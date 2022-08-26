0a48a78c7a20269e47a5f5b905419ec2a6fc418d
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 16:45:46 -0400

    Abstract class serves base create distint types of orders

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/domain/order/AbstractOrder.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/domain/order/AbstractOrder.java
@@ -1,5 +1,9 @@
 package chainofresponsability.domain.order;
 
+import chainofresponsability.domain.Contributor;
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +11,44 @@ package chainofresponsability.domain.order;
  * Time: 3:39 p. m.
  */
 
-public class AbstractOrder {
+public abstract class AbstractOrder {
+
+  private Calendar createDate;
+  private Contributor contributor;
+  private List<OrderItem> orderItems;
+
+  public Calendar getCreateDate(){
+    return createDate;
+  }
+
+  public double getTotal(){
+    double total = 0d;
+    for(OrderItem abstractOrderItem: orderItems){
+      total += abstractOrderItem.getTotal();
+    }
+    return total;
+  }
+
+  // Getters and Setters
+
+
+  public void setCreateDate(Calendar createDate) {
+    this.createDate = createDate;
+  }
+
+  public Contributor getContributor() {
+    return contributor;
+  }
+
+  public void setContributor(Contributor contributor) {
+    this.contributor = contributor;
+  }
+
+  public List<OrderItem> getOrderItems() {
+    return orderItems;
+  }
+
+  public void setOrderItems(List<OrderItem> orderItems) {
+    this.orderItems = orderItems;
+  }
 }
