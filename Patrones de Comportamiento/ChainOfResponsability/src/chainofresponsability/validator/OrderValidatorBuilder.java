bcb474cd021bf6a0de6cb5439aff126f6de6e880
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 17:56:24 -0400

    Class that implements wait utility for create the structure of tree of validation

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/OrderValidatorBuilder.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/OrderValidatorBuilder.java
@@ -8,4 +8,27 @@ package chainofresponsability.validator;
  */
 
 public class OrderValidatorBuilder {
+
+  public static final AbstractOrderValidator buildSalesOrderValidator(){
+    AbstractOrderValidator validator = new SalesOrderValidator();
+    validator.addValidator(buildCustomerValidator());
+    validator.addValidator(new OrderItemValidator());
+    validator.addValidator(new CreditValidator());
+    return validator;
+  }
+
+  private static AbstractOrderValidator buildCustomerValidator(){
+    AbstractOrderValidator validator = new CustomerValidator();
+    validator.addValidator(buildContribuitorValidator());
+    return validator;
+  }
+
+  private static AbstractOrderValidator buildContributorValidator(){
+    AbstractOrderValidator validator = new ContribuitorValidator();
+    validator.addValidator(AddressValidator());
+    validator.addValidator(new TelephoneValidator());
+    return validator;
+  }
+
+
 }
