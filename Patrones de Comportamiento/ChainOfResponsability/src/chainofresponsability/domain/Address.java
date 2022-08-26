c41c23ee6c47b5013d5963b65f15c7f4d102fd0a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 15:59:34 -0400

    Class that corespond to the address of client

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/domain/Address.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/domain/Address.java
@@ -8,4 +8,43 @@ package chainofresponsability.domain;
  */
 
 public class Address {
+  private String address1;
+  private String address2;
+  private String country;
+  private String CP;
+
+  // Getters and Setters
+
+  public String getAddress1(){
+    return this.address1;
+  }
+
+  public void setAddress1(String address1){
+    this.address1 = address1;
+  }
+
+  public String getAddress2(){
+    return this.address2;
+  }
+
+  public void setAddress2(String address2){
+    this.address2 = address2;
+  }
+
+  public String getCountry(){
+    return this.country;
+  }
+
+  public void setCountry(String country){
+    this.country = country;
+  }
+
+  public String getCP(){
+    return this.CP;
+  }
+
+  public void setCP(String CP){
+    this.CP = CP;
+  }
+
 }
