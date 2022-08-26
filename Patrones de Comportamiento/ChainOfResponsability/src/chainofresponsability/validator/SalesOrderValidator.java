e9ff3e2faf88535be395ce21f8e89415bd2b54e9
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 18:06:11 -0400

    Class that extends AbstractOrderValidator and it allows validate the type of order

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/SalesOrderValidator.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/SalesOrderValidator.java
@@ -1,5 +1,8 @@
 package chainofresponsability.validator;
 
+import chainofresponsability.domain.order.AbstractOrder;
+import chainofresponsability.domain.order.SalesOrder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,17 @@ package chainofresponsability.validator;
  * Time: 3:46 p. m.
  */
 
-public class SalesOrderValidator {
+public class SalesOrderValidator extends AbstractOrderValidator{
+  @Override
+  public void validate(AbstractOrder order) throws ValidationException {
+
+    if(!(order instanceof SalesOrder)){
+      throw new ValidationException("Se esperaba una orden de venta");
+    }
+
+    for(AbstractOrderValidator validator: validators){
+      validator.validate(order);
+    }
+
+  }
 }
