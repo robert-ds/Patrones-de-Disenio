695fa79d4a9c3a36a7e9a0b8b60a3362f5bdf6cd
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-08-26 18:02:11 -0400

    Class that extends AbstractOrderValidator and will be used for validate the telephone of client

--- Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/TelephoneValidator.java
+++ Patrones de Comportamiento/ChainOfResponsability/src/chainofresponsability/validator/TelephoneValidator.java
@@ -1,5 +1,8 @@
 package chainofresponsability.validator;
 
+import chainofresponsability.domain.Telephone;
+import chainofresponsability.domain.order.AbstractOrder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,18 @@ package chainofresponsability.validator;
  * Time: 3:46 p. m.
  */
 
-public class TelephoneValidator {
+public class TelephoneValidator extends AbstractOrderValidator {
+  @Override
+  public void validate(AbstractOrder order) throws ValidationException {
+    Telephone tel = order.getContributor().getTelephone();
+
+    if(null == tel){
+      throw new ValidationException("El Telefono del contribuyente es obligatorio");
+    }else if(tel.getNumber().length() != 7){
+      throw new ValidationException("Número de Teléfo invalido");
+    }else if(tel.getLada().length() != 3){
+      throw new ValidationException("Lada invalida");
+    }
+
+  }
 }
