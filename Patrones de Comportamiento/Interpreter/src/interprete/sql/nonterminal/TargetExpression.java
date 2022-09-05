7cf269e145e4c7a95ec8ee8f065c22f39bf78bd0
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-05 18:26:48 -0400

    This class interpret the columns selected of the table and represent columns select

--- Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/TargetExpression.java
+++ Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/TargetExpression.java
@@ -1,5 +1,16 @@
 package interprete.sql.nonterminal;
 
+import interprete.sql.AbstractSQLExpression;
+import interprete.sql.Context;
+import interprete.sql.InterpreteException;
+import interprete.sql.terminal.LiteralExpression;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +18,45 @@ package interprete.sql.nonterminal;
  * Time: 3:01 p. m.
  */
 
-public class TargetExpression {
+public class TargetExpression implements AbstractSQLExpression {
+
+  private List<LiteralExpression> targets = new ArrayList<LiteralExpression>();
+
+  public TargetExpression(LiteralExpression... expressions){
+    this.targets = Arrays.asList(expressions);
+  }
+
+  @Override
+  public void interpret(Context context) throws InterpreteException {
+    context.createResultArray(targets.size());
+
+    List<Row> resultRow = context.getResultRow();
+
+    for(Row row: resultRow){
+
+      Object[] result = context.createRow();
+      int col = 0;
+
+      for(LiteralExpression literalExpression: targets){
+        String column = literalExpression.interpret(context).toString();
+        context.tableColumn(column);
+        int columnIndex = context.columnIndex(column);
+        Cell cell = row.getCell(columnIndex);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        String value = cell.getStringCellValue();
+        result[col++] = value;
+      }
+    }
+
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    String output = "";
+    for(LiteralExpression literalExpression: targets){
+      output += ", " + literalExpression.toString();
+    }
+    return output.substring(2);
+  }
 }
