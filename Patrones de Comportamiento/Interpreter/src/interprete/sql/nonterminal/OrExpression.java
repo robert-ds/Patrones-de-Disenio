442b225073d8a781ca8d3529fc3407c19f9db9fe
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-05 19:10:02 -0400

    This class represent the instruction OR of SQL

--- Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/OrExpression.java
+++ Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/OrExpression.java
@@ -1,5 +1,8 @@
 package interprete.sql.nonterminal;
 
+import interprete.sql.Context;
+import interprete.sql.InterpreteException;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,26 @@ package interprete.sql.nonterminal;
  * Time: 2:59 p. m.
  */
 
-public class OrExpression {
+public class OrExpression implements StatementExpression{
+
+  private StatementExpression leftStatement;
+  private StatementExpression rightStatement;
+
+  public OrExpression(StatementExpression leftStatement, StatementExpression rightStatement){
+    this.leftStatement = leftStatement;
+    this.rightStatement = rightStatement;
+  }
+
+  @Override
+  public Object interpret(Context context) throws InterpreteException {
+    Boolean leftReslt = (Boolean) leftStatement.interpret(context);
+    Boolean rightReslt = (Boolean) rightStatement.interpret(context);
+    return leftReslt || rightReslt;
+  }
+
+  @Override
+  public String toString() {
+    return "( " + leftStatement.toString() +
+        " OR" + rightStatement.toString() + " )";
+  }
 }
