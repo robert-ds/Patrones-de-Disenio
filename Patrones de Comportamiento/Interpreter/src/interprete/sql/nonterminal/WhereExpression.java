b41f5b40c7fc130d44828285f7559fc6e9a21131
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-05 18:09:11 -0400

    The class WhereExpression represent all the section of 'WHERE'

--- Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/WhereExpression.java
+++ Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/WhereExpression.java
@@ -1,5 +1,9 @@
 package interprete.sql.nonterminal;
 
+import interprete.sql.AbstractSQLExpression;
+import interprete.sql.Context;
+import interprete.sql.InterpreteException;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +11,43 @@ package interprete.sql.nonterminal;
  * Time: 3:01 p. m.
  */
 
-public class WhereExpression {
+public class WhereExpression implements AbstractSQLExpression {
+
+  private StatementExpression statement;
+
+  public WhereExpression(StatementExpression statement){
+    this.statement = statement;
+  }
+
+  @Override
+  public void interpret(Context context) throws InterpreteException {
+    context.createRowIterator();
+
+    while(context.nextRow()){
+
+      if(statement == null){
+        context.addCurrentRowToResults();
+      }else{
+        Boolean result = (Boolean) statement.interpret(context);
+
+        if(result){
+          context.addCurrentRowToResults();
+        }
+
+      }
+
+    }
+
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    if(statement != null){
+      return "WHERE" + statement.toString();
+    }else{
+      return "";
+    }
+
+  }
 }
