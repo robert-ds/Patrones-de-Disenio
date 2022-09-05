364ea378c1e753ac5482e4f6d5875afe7c03a257
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-05 17:24:11 -0400

    This class indicate the interpreter that the contains is a date and not just text

--- Patrones de Comportamiento/Interpreter/src/interprete/sql/terminal/DateExpression.java
+++ Patrones de Comportamiento/Interpreter/src/interprete/sql/terminal/DateExpression.java
@@ -1,5 +1,10 @@
 package interprete.sql.terminal;
 
+import interprete.sql.Context;
+import interprete.sql.InterpreteException;
+
+import java.text.SimpleDateFormat;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +12,26 @@ package interprete.sql.terminal;
  * Time: 3:02 p. m.
  */
 
-public class DateExpression {
+public class DateExpression extends LiteralExpression {
+
+  public DateExpression(String literal){
+    super(literal);
+  }
+
+  @Override
+  public Object interpret(Context context) throws InterpreteException {
+
+    try{
+      SimpleDateFormat dateFormat = context.getDateFormat();
+      return dateFormat.parse(literal);
+    }catch(Exception e){
+      throw new InterpreteException("Formato de fecha inválido" + literal + "");
+    }
+
+  }
+
+  @Override
+  public String toString() {
+    return "" + literal + "";
+  }
 }
