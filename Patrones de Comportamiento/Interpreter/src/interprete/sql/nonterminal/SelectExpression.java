d48253ae0b77524a82b884f9cf1f9d593cfb379f
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-05 17:48:10 -0400

    This class represent a select of SQL

--- Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/SelectExpression.java
+++ Patrones de Comportamiento/Interpreter/src/interprete/sql/nonterminal/SelectExpression.java
@@ -1,5 +1,11 @@
 package interprete.sql.nonterminal;
 
+import interprete.sql.AbstractSQLExpression;
+import interprete.sql.Context;
+import interprete.sql.InterpreteException;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +13,39 @@ package interprete.sql.nonterminal;
  * Time: 3:00 p. m.
  */
 
-public class SelectExpression {
+public class SelectExpression implements AbstractSQLExpression {
+
+  private final TargetExpression target;
+  private final FromExpression from;
+  private final WhereExpression where;
+
+  public SelectExpression(TargetExpression columns, FromExpression table, WhereExpression where){
+    this.target = columns;
+    this.from = table;
+    this.where = where;
+  }
+
+  public SelectExpression(TargetExpression columns, FromExpression table){
+    this.target = columns;
+    this.from = table;
+    this.where = new WhereExpression(null);
+  }
+
+  @Override
+  public List<Object[]> interpret(Context context) throws InterpreteException {
+
+    from.interpret(context);
+    where.interpret(context);
+    target.interpret(context);
+    return context.getResultArray();
+
+  }
+
+  @Override
+  public String toString() {
+    return "Select " +
+        "target=" + target.toString() +
+        ", from=" + from.toString() +
+        ", where=" + where.toString();
+  }
 }
