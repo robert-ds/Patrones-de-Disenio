808c8c55e0a0dd0b1e2edaa1bcabd2a4c5b670d6
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 17:20:22 -0400

    Class that is used for request the create of a purchase order

--- Patrones de Comportamiento/mediator/src/mediator/module/impl/dto/ProductRequest.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/impl/dto/ProductRequest.java
@@ -1,5 +1,8 @@
 package mediator.module.impl.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,4 +11,15 @@ package mediator.module.impl.dto;
  */
 
 public class ProductRequest {
+
+  public List<Product> products = new ArrayList<>();
+
+  public List<Product> getProducts(){
+    return this.products;
+  }
+
+  public void setProducts(List<Product> products){
+    this.products = products;
+  }
+
 }
