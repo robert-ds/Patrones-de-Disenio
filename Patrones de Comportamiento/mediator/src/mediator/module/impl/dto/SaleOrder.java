a287562d323d61d2b878f8fd95d329ce35baab05
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 17:23:38 -0400

    Class that is used for create the sale order

--- Patrones de Comportamiento/mediator/src/mediator/module/impl/dto/SaleOrder.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/impl/dto/SaleOrder.java
@@ -7,5 +7,22 @@ package mediator.module.impl.dto;
  * Time: 4:33 p. m.
  */
 
-public class SaleOrder {
+public class SaleOrder extends Sale{
+
+  private String id;
+
+  public SaleOrder(String id){
+    this.id = id;
+  }
+
+  // Getters and Setters
+
+  public String getId(){
+    return this.id;
+  }
+
+  public void setId(String id){
+    this.id = id;
+  }
+
 }
