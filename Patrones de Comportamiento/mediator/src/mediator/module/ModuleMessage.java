637b7ce8e20a42ea02370cfe8fccf9653a147d77
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 17:06:25 -0400

    This class is complement of the class ModuleMediator

--- Patrones de Comportamiento/mediator/src/mediator/module/ModuleMessage.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/ModuleMessage.java
@@ -8,4 +8,48 @@ package mediator.module;
  */
 
 public class ModuleMessage {
+
+  private String source;
+  private String target;
+  private String messageType;
+  private Object payload;
+
+  public ModuleMessage(String source, String target, String messageType, Object payload){
+    this.source = source;
+    this.target = target;
+    this.messageType = messageType;
+    this.payload = payload;
+  }
+
+  public String getSource(){
+    return this.source;
+  }
+
+  public void setSource(String source){
+    this.source = source;
+  }
+
+  public Object getPayLoad(){
+    return this.payload;
+  }
+
+  public void setPayLoad(Object payload){
+    this.payload = payload;
+  }
+
+  public String getTarget() {
+    return this.target;
+  }
+
+  public void setTarget(String target) {
+    this.target = target;
+  }
+
+  public String getMessageType() {
+    return this.messageType;
+  }
+
+  public void setMessageType(String messageType) {
+    this.messageType = messageType;
+  }
 }
