e8d7d064d8843fbeef61b3f86ddd7a0da319d7b1
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 18:12:25 -0400

    This class represent the module of store

--- Patrones de Comportamiento/mediator/src/mediator/module/impl/StockModule.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/impl/StockModule.java
@@ -1,5 +1,11 @@
 package mediator.module.impl;
 
+import mediator.module.AbstractModule;
+import mediator.module.ModuleMessage;
+import mediator.module.impl.dto.Product;
+import mediator.module.impl.dto.ProductRequest;
+import mediator.module.impl.dto.SaleOrder;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +13,42 @@ package mediator.module.impl;
  * Time: 4:31 p. m.
  */
 
-public class StockModule {
+public class StockModule extends AbstractModule {
+
+  public static final String MODULE_NAME = "Stock";
+  public static final String OPERATION_DECREMENT_STOCK = "DecrementStock";
+
+
+  @Override
+  public String getModulName() {
+    return MODULE_NAME;
+  }
+
+  @Override
+  public Object notifyMessage(ModuleMessage message) {
+    switch(message.getMessageType()){
+      case OPERATION_DECREMENT_STOCK:
+        return decrementStock(message);
+
+      default:
+        throw new RuntimeException("Operation no soportada " + message.getMessageType() + "");
+    }
+  }
+
+  private void decrementStock(ModuleMessage message){
+    SaleOrder saleOrder = (SaleOrder) message.getPayLoad();
+
+    for(Product product: saleOrder.getProductos()){
+      System.out.println("decrement product >" + product.getName());
+    }
+
+    ProductRequest productRequest = new ProductRequest();
+    productRequest.setProducts(saleOrder.getProductos());
+
+    ModuleMessage purchaseMessage = new ModuleMessage(MODULE_NAME, PurchaseModule.MODULE_NAME, PurchaseModule.OPERATION_PURCHASE_REQUEST, productRequest);
+    mediator.mediate(purchaseMessage);
+    return null;
+
+  }
+
 }
