5012a9a115ff07ad37756c0e2919b6ce5dae304e
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 16:41:27 -0400

    Class that represent a module of form abstract

--- Patrones de Comportamiento/mediator/src/mediator/module/AbstractModule.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/AbstractModule.java
@@ -7,5 +7,17 @@ package mediator.module;
  * Time: 4:27 p. m.
  */
 
-public class AbstractModule {
+public abstract class AbstractModule {
+
+  protected ModuleMediator mediator;
+
+  public abstract String getModulName();
+
+  public void activate(){
+    mediator = ModuleMediator.getInstance();
+    mediator.registModule(this);
+  }
+
+  public abstract Object notifyMessage(ModuleMessage message);
+
 }
