07360ed4c8aa0e58cd7ea9d4e7a93f2516dbc27a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 18:25:53 -0400

    This class represent the module of notify

--- Patrones de Comportamiento/mediator/src/mediator/module/impl/NotifyModule.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/impl/NotifyModule.java
@@ -1,5 +1,8 @@
 package mediator.module.impl;
 
+import mediator.module.AbstractModule;
+import mediator.module.ModuleMessage;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,5 +10,31 @@ package mediator.module.impl;
  * Time: 4:30 p. m.
  */
 
-public class NotifyModule {
+public class NotifyModule extends AbstractModule {
+
+  public static final String MODULE_NAME = "Notificación";
+  public static final String OPERATION_NOTIFY = "Notify";
+
+
+  @Override
+  public String getModulName() {
+    return MODULE_NAME;
+  }
+
+  @Override
+  public Void notifyMessage(ModuleMessage message) {
+    switch(message.getMessageType()){
+      case OPERATION_NOTIFY:
+        return notify(message);
+
+      default:
+        throw new RuntimeException("Operation no soprtada " + message.getMessageType() + "");
+    }
+  }
+
+  private Void notify(ModuleMessage message){
+    System.out.println("Notificación por Email Enviada");
+    return null;
+  }
+
 }
