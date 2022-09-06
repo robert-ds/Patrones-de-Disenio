3eda0017eee8c0a5712c3c119ba4bc6dbf406c68
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-06 16:58:01 -0400

    This class is the angular part of this example, since it will be the that comunicate between all modules

--- Patrones de Comportamiento/mediator/src/mediator/module/ModuleMediator.java
+++ Patrones de Comportamiento/mediator/src/mediator/module/ModuleMediator.java
@@ -1,5 +1,8 @@
 package mediator.module;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,5 +11,34 @@ package mediator.module;
  */
 
 public class ModuleMediator {
+
+  private static ModuleMediator mediator;
+
+  private static final Map<String, AbstractModule> modules = new HashMap<>();
+
+  private ModuleMediator(){}
+
+  public static synchronized ModuleMediator getInstance(){
+    if(mediator == null){
+      mediator = new ModuleMediator();
+    }
+    return mediator;
+  }
+
+  public void registModule(AbstractModule module){
+    modules.put(module.getModulName(),module);
+  }
+
+  public Object mediate(ModuleMessage mediateEvent){
+
+    if(!modules.containsKey(mediateEvent.getTarget())){
+      throw new RuntimeException("El módulo " + mediateEvent.getTarget() + " no está registrado");
+    }
+
+    System.out.println("Mediate source > " + mediateEvent.getSource() + ", target > " + mediateEvent.getTarget() + ", messageType > " + mediateEvent.getMessageType() + "");
+    AbstractModule module = modules.get(mediateEvent.getTarget());
+    return module.notifyMessage(mediateEvent);
+
+  }
 
 }
