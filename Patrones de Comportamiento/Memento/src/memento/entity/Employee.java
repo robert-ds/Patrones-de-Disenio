e208cb6aef52cc1c7803ec6598ae6d1301f708b4
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-07 16:12:59 -0400

    The class Employee represent to the Originator

--- Patrones de Comportamiento/Memento/src/memento/entity/Employee.java
+++ Patrones de Comportamiento/Memento/src/memento/entity/Employee.java
@@ -1,5 +1,7 @@
 package memento.entity;
 
+import memento.impl.EmployeeMemento;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -7,6 +9,60 @@ package memento.entity;
  * Time: 3:51 p. m.
  */
 
-public class Employee {
+public class Employee implements Cloneable {
+
+  private String name;
+  private String lastName;
+  private String employeeNumber;
+
+  public Employee(String name, String lastName, String employeeNumber){
+    this.name = name;
+    this.lastName = lastName;
+    this.employeeNumber = employeeNumber;
+  }
+
+  public Employee(){}
+
+  // Getter and Setters
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public String getLastName(){
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName){
+    this.lastName = lastName;
+  }
+
+  public String getEmployeeNumber(){
+    return this.employeeNumber;
+  }
+
+  public void setEmployeeNumber(String employeeNumber){
+    this.employeeNumber = employeeNumber;
+  }
+
+  public EmployeeMemento createMemento(){
+    try{
+      return new EmployeeMemento((Employee)this.clone());
+    }catch(Exception e){
+      throw new RuntimeException("Error al clonar el empleado");
+    }
+  }
+
+  public void restoreMemento(EmployeeMemento memento){
+    Employee employee = memento.getMemento();
+    this.name = employee.name;
+    this.lastName = employee.name;
+    this.employeeNumber = employeeNumber;
+  }
+
 
 }
