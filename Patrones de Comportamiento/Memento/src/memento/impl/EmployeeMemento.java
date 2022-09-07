98817741f9a9ba86c150169693330f07321ec73a
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-07 16:17:47 -0400

    Class used for save a stade of empoyee

--- Patrones de Comportamiento/Memento/src/memento/impl/EmployeeMemento.java
+++ Patrones de Comportamiento/Memento/src/memento/impl/EmployeeMemento.java
@@ -1,11 +1,24 @@
 package memento.impl;
 
+import memento.entity.Employee;
+
 /**
  * Created with IntelliJ IDEA
- * Created By Robert Vasquez
+ * Created By Robert Vásquez
  * Date: 7/9/22
  * Time: 3:57 p. m.
  */
 
 public class EmployeeMemento {
+
+  public Employee employee;
+
+  public EmployeeMemento(Employee employee){
+    this.employee = employee;
+  }
+
+  public Employee getMemento(){
+    return employee;
+  }
+
 }
