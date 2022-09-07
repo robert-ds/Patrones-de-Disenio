af2d35a11566e810bb09fbeea5a26e62bf3adb3e
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-07 16:28:56 -0400

    This class represent the Caretaker which is responsible of save the states

--- Patrones de Comportamiento/Memento/src/memento/impl/EmployeeCaretaker.java
+++ Patrones de Comportamiento/Memento/src/memento/impl/EmployeeCaretaker.java
@@ -1,5 +1,7 @@
 package memento.impl;
 
+import java.util.LinkedList;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
@@ -8,4 +10,41 @@ package memento.impl;
  */
 
 public class EmployeeCaretaker {
+
+  private int currentIndex = -1;
+
+  private final LinkedList<EmployeeMemento> states = new LinkedList<>();
+
+  public void addNewMemento(EmployeeMemento m){
+    states.add(m);
+    currentIndex++;
+  }
+
+  public EmployeeMemento getCurrentMemento(){
+    return states.get(currentIndex);
+  }
+
+  public EmployeeMemento getNextMemento(){
+    int newIndex = currentIndex + 1;
+
+    if(newIndex >= states.size()){
+      return null;
+    }
+
+    currentIndex = newIndex;
+    return states.get(currentIndex);
+  }
+
+  public EmployeeMemento getPreviousMemento(){
+    int newIndex = currentIndex - 1;
+
+    if(newIndex <= -1 || newIndex >= states.size() -1){
+      return null;
+    }
+
+    currentIndex = newIndex;
+    return states.get(currentIndex);
+
+  }
+
 }
