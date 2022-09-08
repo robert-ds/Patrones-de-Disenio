ce199de2581005a8820ecb716a7eb61f8b41937b
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-09-08 15:47:06 -0400

    The class Employee is the class more important of project

--- Patrones de Comportamiento/NullObject/src/nullobject/domain/Employee.java
+++ Patrones de Comportamiento/NullObject/src/nullobject/domain/Employee.java
@@ -8,4 +8,60 @@ package nullobject.domain;
  */
 
 public class Employee {
+
+  private Long id;
+  private String name;
+  private Address address;
+
+  public Employee(Long id, String name, Address address){
+    this.id = id;
+    this.name = name;
+    this.address = address;
+  }
+
+  public Employee(){}
+
+  // Getters and Setters
+
+  public Long getId(){
+    return this.id;
+  }
+
+  public void setId(Long id){
+    this.id = id;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public Address getAddress(){
+    return this.address;
+  }
+
+  public void setAddress(Address address){
+    this.address = address;
+  }
+
+
+  public static final Employee NULL_EMPLOYEE = new Employee(){
+
+    public Long getId(){
+      return 0L;
+    }
+
+    public String getName(){
+      return "UNKNOW EMPLOYEE";
+    }
+
+    public Address getAddress(){
+      return Address.NULL_ADDRESS;
+    }
+
+  }
+
 }
